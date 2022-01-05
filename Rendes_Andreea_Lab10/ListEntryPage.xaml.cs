6cadbe5b0cdd816b486621ffa88bf573b44fd3ac
Andreea Rendes <andreearendes@outlook.com>
2022-01-05 18:04:54 +0200

    Laborator11

--- Rendes_Andreea_Lab10/ListEntryPage.xaml.cs
+++ Rendes_Andreea_Lab10/ListEntryPage.xaml.cs
@@ -40,5 +40,6 @@ namespace Rendes_Andreea_Lab10
                 });
             }
         }
+
     }
 }
--- Rendes_Andreea_Lab10/ListPage.xaml.cs
+++ Rendes_Andreea_Lab10/ListPage.xaml.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Rendes_Andreea_Lab10.Models;
-
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -30,6 +29,22 @@ namespace Rendes_Andreea_Lab10
             var slist = (ShopList)BindingContext;
             await App.Database.DeleteShopListAsync(slist);
             await Navigation.PopAsync();
+        }
+        async void OnChooseButtonClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ProductPage((ShopList)
+                this.BindingContext)
+            {
+                BindingContext = new Product()
+            });
+
+        }
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            var shopl = (ShopList)BindingContext;
+
+            listView.ItemsSource = await App.Database.GetListProductsAsync(shopl.ID);
         }
     }
 }
